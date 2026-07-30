9c01d7a05a50e780000b20b573eccb5d631c562a
Lucas Damián <lukeiner.dev@gmail.com>
2026-07-30 12:14:47 -0300

    Experience Creation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float velocidad = 5f; //velocidad del jugador
+    private Rigidbody2D rb2d;
+    private float movimiento;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moverX = Input.GetAxis("Horizontal");
+        float moverY = Input.GetAxis("Vertical");
+
+        rb2d.linearVelocity = new Vector2(moverX * velocidad, moverY * velocidad);
+    }
+}
