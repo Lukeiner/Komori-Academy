757ffb9755b3fc6122a04feb128b061c0db35581
Lucas Damián <lukeiner.dev@gmail.com>
2026-08-03 20:26:34 -0300

    Update 1.3
    Added animation to player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,30 @@ public class Player : MonoBehaviour
     public float velocidad = 5f; //velocidad del jugador
     private Rigidbody2D rb2d;
     private float movimiento;
+    [SerializeField] private Animator animator;
     [SerializeField] private AudioSource musiquita;
-
+    private Vector2 movementInput;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
-
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float moverX = Input.GetAxis("Horizontal");
-        float moverY = Input.GetAxis("Vertical");
+        //float moverX = Input.GetAxis("Horizontal");
+        //float moverY = Input.GetAxis("Vertical");
 
-        rb2d.linearVelocity = new Vector2(moverX * velocidad, moverY * velocidad);
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+        //rb2d.linearVelocity = new Vector2(moverX * velocidad, moverY * velocidad);
+
+        animator.SetFloat("Horizontal", movementInput.x);
+        animator.SetFloat("Speed", movementInput.magnitude);
+    }
+    private void FixedUpdate()
+    {
+        rb2d.linearVelocity = movementInput * velocidad;
     }
 }
